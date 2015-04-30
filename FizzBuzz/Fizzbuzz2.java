a6de153f1b3b297a0470408be2735a64a6fec1c5
Zane Pocock <zanepocock@gmail.com>
2015-05-01 09:36:20 +1200

    Corrected cascade
    Moved if (i % 15 == 0) to start in order for program to run properly.

--- FizzBuzz/Fizzbuzz2.java
+++ FizzBuzz/Fizzbuzz2.java
@@ -4,14 +4,14 @@ public class Fizzbuzz2 {
     public static void main(String[] args){
         String buzz = "buzz", fizz = "fizz";
         for(int i = 0; i < 100; i++){
+            if (i % 15 == 0){
+                System.out.println(fizz + buzz);
+            } else
             if (i % 3 == 0){
                 System.out.println(fizz);
             } else 
             if (i % 5 == 0){
                 System.out.println(buzz);
-            } else
-            if (i % 15 == 0){
-                System.out.println(fizz + buzz);
             } else {
                 System.out.println(i);
             }
